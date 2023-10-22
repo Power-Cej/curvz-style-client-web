a353f148bd5133d55742ca0ccaa2bc83af401c84
ceejay900 <58793044+ceejay900@users.noreply.github.com>
2023-10-22 09:23:58 +0800

    enable timer

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,11 @@ import './App.css';
 const App = () => {
   const videoRef = useRef(null);
   const canvasRef = useRef(null);
+
+  const [timer, setTimer] = useState(null);
   const [data, setData] = useState(undefined);
+
+  const [timerActive] = useState(false); // Added timerActive state
 
   const startCamera = async () => {
     try {
@@ -16,8 +20,22 @@ const App = () => {
   };
 
   useEffect(() => {
-    startCamera()
-  }, [])
+    startCamera();
+  }, []);
+
+  const toggleTimer = () => {
+    if (!timer) {
+      setTimer(5);
+      const timerInterval = setInterval(() => {
+        setTimer(prevTimer => prevTimer - 1);
+      }, 1000);
+
+      setTimeout(async () => {
+        clearInterval(timerInterval);
+        takePicture();
+      }, 5000);
+    }
+  };
 
   const takePicture = () => {
     const context = canvasRef.current.getContext('2d');
@@ -31,39 +49,46 @@ const App = () => {
       },
       body: JSON.stringify({ imageData }),
     })
-      .then((response) => response.json())
-      .then((data) => {
-        setData(data)
+      .then(response => response.json())
+      .then(data => {
+        setData(data);
         console.log(data);
       })
-      .catch((error) => console.error('Error uploading picture:', error));
+      .catch(error => console.error('Error uploading picture:', error));
   };
 
   return (
-    <div>
+    <div className="content">
       <h1>React Camera</h1>
-      <div className='rectangle'></div>
+      <div className="rectangle"></div>
       <video ref={videoRef} width="640" height="480" autoPlay />
       <canvas ref={canvasRef} style={{ display: 'none' }} width="640" height="480" />
-      <div>
-        {/* <button onClick={startCamera}>Start Camera</button> */}
-        <button onClick={takePicture}>Take Picture</button>
+      <div style={{ margin: 20 }}>
+        {/* Display Timer */}
+        {timer !== null && timer !== 0 && <p>{timer} seconds remaining</p>}
+
+        {/* Button to Toggle Timer */}
+        <button onClick={toggleTimer} disabled={timerActive}>
+          <i className={`fas ${timer ? 'fa-toggle-on' : 'fa-toggle-off'}`} />
+          5s Timer
+        </button>
+
+        {/* Button to Take Picture */}
+        <button onClick={takePicture} disabled={timerActive}>
+          <i className="fas fa-camera" /> Take Picture
+        </button>
+
+        {/* Display Result */}
+        {data === undefined ? (
+          <p>Loading...</p>
+        ) : (
+          <ul>
+            <li>Shoulder: {data.shoulder_width_cm}</li>
+            <li>Top: {data.top_length_cm}</li>
+            <li>Leg: {data.leg_length_cm}</li>
+          </ul>
+        )}
       </div>
-      {/* display result  */}
-      {
-        (data === undefined) ?
-          (
-            <p>Loading...</p>
-          )
-          :
-          (
-            <ul>
-              <li>Shoulder: {data.shoulder_width_cm}</li>
-              <li>Top: {data.top_length_cm}</li>
-              <li>Leg: {data.leg_length_cm}</li>
-            </ul>
-          )
-      }
     </div>
   );
 };
